0b9d1b5df6fb75aa4dc68d0798a450894c564e41
TurtysProductions <43997792+TurtysProductions@users.noreply.github.com>
2020-06-29 12:59:52 +0100

    Test classes

---
+++ src/main/java/com/turtywurty/turtyschemistry/core/util/ModTags.java
@@ -0,0 +1,12 @@
+package com.turtywurty.turtyschemistry.core.util;
+
+import com.turtywurty.turtyschemistry.TurtyChemistry;
+
+import net.minecraft.item.Item;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
+
+public class ModTags {
+
+	public static final Tag<Item> SAWDUST = new Tag<Item>(new ResourceLocation(TurtyChemistry.MOD_ID, "sawdust"));
+}
