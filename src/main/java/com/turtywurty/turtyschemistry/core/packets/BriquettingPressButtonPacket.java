862e4bd7bc6974a5bd63e142517042182ef489d8
TurtysProductions <43997792+TurtysProductions@users.noreply.github.com>
2020-06-29 12:54:53 +0100

    Create BriquettingPressButtonPacket.java

---
+++ src/main/java/com/turtywurty/turtyschemistry/core/packets/BriquettingPressButtonPacket.java
@@ -0,0 +1,41 @@
+package com.turtywurty.turtyschemistry.core.packets;
+
+import java.util.function.Supplier;
+
+import com.turtywurty.turtyschemistry.common.container.BriquettingPressContainer;
+
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+public class BriquettingPressButtonPacket {
+
+	private boolean buttonPressed;
+
+	public BriquettingPressButtonPacket(boolean buttonPressedIn) {
+		this.buttonPressed = buttonPressedIn;
+	}
+
+	public void encode(PacketBuffer packetBuffer) {
+		packetBuffer.writeByte(buttonPressed ? 1 : 0);
+	}
+
+	public static BriquettingPressButtonPacket decode(PacketBuffer buffer) {
+		return new BriquettingPressButtonPacket(buffer.readByte() > 0);
+	}
+
+	public void onRecieved(Supplier<NetworkEvent.Context> context) {
+		context.get().enqueueWork(() -> {
+			if (context.get().getSender().openContainer instanceof BriquettingPressContainer) {
+				BriquettingPressContainer container = (BriquettingPressContainer) context.get()
+						.getSender().openContainer;
+				container.tileEntity.setPressed(this.buttonPressed);
+				container.tileEntity.getWorld().notifyBlockUpdate(container.tileEntity.getPos(),
+						container.tileEntity.getBlockState(), container.tileEntity.getBlockState(),
+						Constants.BlockFlags.DEFAULT);
+			}
+
+		});
+		context.get().setPacketHandled(true);
+	}
+}
