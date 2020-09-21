72cda3155b50c3f83f4f3eb80b354ec990e63045
bernie-g <berniegandin@gmail.com>
2020-09-21 12:39:20 -0400

    Fixed texture replacement I think

--- src/main/java/com/turtywurty/turtyschemistry/client/model/ExistingModel.java
+++ src/main/java/com/turtywurty/turtyschemistry/client/model/ExistingModel.java
@@ -1,10 +1,5 @@
 package com.turtywurty.turtyschemistry.client.model;
 
-import java.util.List;
-import java.util.Random;
-
-import com.turtywurty.turtyschemistry.client.util.ClientUtils;
-
 import net.minecraft.block.BlockState;
 import net.minecraft.client.renderer.model.BakedQuad;
 import net.minecraft.client.renderer.model.IBakedModel;
@@ -12,50 +7,86 @@ import net.minecraft.client.renderer.model.ItemOverrideList;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.util.Direction;
 
-public class ExistingModel implements IBakedModel {
+import java.util.*;
+
+public class ExistingModel implements IBakedModel
+{
 
 	private final IBakedModel model;
 	private final TextureAtlasSprite texture;
+	private List<BakedQuad> generalQuads;
+	private Map<Direction, List<BakedQuad>> sideQuads = new HashMap<>();
 
-	public ExistingModel(IBakedModel modelIn, TextureAtlasSprite textureIn) {
+
+	public ExistingModel(IBakedModel modelIn, TextureAtlasSprite textureIn)
+	{
 		this.model = modelIn;
 		this.texture = textureIn;
+
+		generalQuads = modelIn.getQuads(null, null, null);
+		Arrays.stream(Direction.values()).forEach(dir -> sideQuads.put(dir, modelIn.getQuads(null, dir, null)));
+
+		for (int i = 0; i < generalQuads.size(); i++)
+		{
+			BakedQuad quad = generalQuads.get(i);
+			generalQuads.set(i, new BakedQuad(quad.getVertexData(), quad.getTintIndex(), quad.getFace(), textureIn,
+					quad.shouldApplyDiffuseLighting()));
+		}
+
+		for (Map.Entry<Direction, List<BakedQuad>> quadSide : sideQuads.entrySet())
+		{
+			List<BakedQuad> value = quadSide.getValue();
+			for (int i = 0; i < value.size(); i++)
+			{
+				BakedQuad quad = value.get(i);
+				value.set(i, new BakedQuad(quad.getVertexData(), quad.getTintIndex(), quad.getFace(), textureIn,
+						quad.shouldApplyDiffuseLighting()));
+			}
+		}
+		int i = 34;
 	}
 
 	@Override
-	public boolean isGui3d() {
+	public boolean isGui3d()
+	{
 		return model.isGui3d();
 	}
 
 	@Override
-	public boolean isBuiltInRenderer() {
+	public boolean isBuiltInRenderer()
+	{
 		return model.isBuiltInRenderer();
 	}
 
 	@Override
-	public boolean isAmbientOcclusion() {
+	public boolean isAmbientOcclusion()
+	{
 		return model.isAmbientOcclusion();
 	}
 
 	@Override
-	public List<BakedQuad> getQuads(BlockState state, Direction side, Random rand) {
-		return ClientUtils.getNewQuads(model, state, side, rand, texture);
+	public List<BakedQuad> getQuads(BlockState state, Direction side, Random rand)
+	{
+		return side == null ? this.generalQuads : this.sideQuads.get(side);
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
-	public TextureAtlasSprite getParticleTexture() {
+	public TextureAtlasSprite getParticleTexture()
+	{
 		return model.getParticleTexture();
 	}
 
 	@Override
-	public ItemOverrideList getOverrides() {
+	public ItemOverrideList getOverrides()
+	{
 		return model.getOverrides();
 	}
 
 	//isSideLit
 	@Override
-	public boolean func_230044_c_() {
+	public boolean func_230044_c_()
+	{
 		return model.func_230044_c_();
 	}
 }
